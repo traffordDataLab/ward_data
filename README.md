3901b07fc5061f75899d5500606426233db3b846
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-12-11 13:45:22 +0000

    Resident qualifications

--- README.md
+++ README.md
@@ -24,7 +24,8 @@
 
 | Indicator     | Period        | Geography     | Source        | &nbsp;        | &nbsp;         |
 |:------------- |:------------- |:------------- |:------------- |:------------- | :------------- |
-|  |  |  |  |  |  |  |
+| Residents with no qualifications | 2011 | 2011 census merged wards | 2011 Census | [data](education/residents_no_qualifications.csv) | [data](education/R/residents_no_qualifications.R) |
+| Residents with a higher education qualification | 2011 | 2011 census merged wards | 2011 Census | [data](education/residents_higher_education_qualification.csv) | [data](education/R/residents_higher_education_qualification.R) |
 
 ### Environment
 
---
+++ education/R/residents_higher_education_qualification.R
@@ -0,0 +1,17 @@
+# Education: Residents with a higher education qualification, 2011 #
+
+# Source: Table KS501UK, ONS 2011 Census
+# URL: https://www.nomisweb.co.uk/census/2011/ks501uk
+# Licence: Open Government Licence
+
+df <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_1510_1.data.csv?date=latest&geography=989862149...989862169&cell=1&measures=20301&select=date_name,geography_name,geography_code,cell_name,measures_name,obs_value,obs_status_name") %>% 
+  select(area_code = GEOGRAPHY_CODE,
+         area_name = GEOGRAPHY_NAME,
+         value = OBS_VALUE) %>% 
+  mutate(period = "2011",
+         indicator = "Residents with a higher education qualification",
+         measure = "percentage",
+         unit = "persons") %>% 
+  select(area_code, area_name, indicator, period, measure, unit, value)
+
+write_csv(df, "../residents_higher_education_qualification.csv")
