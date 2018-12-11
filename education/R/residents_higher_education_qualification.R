975d5ef26be3b31b56dc25d3292c9aafb7bc5899
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-12-11 13:47:20 +0000

    Update residents_higher_education_qualification.R

--- education/R/residents_higher_education_qualification.R
+++ education/R/residents_higher_education_qualification.R
@@ -4,7 +4,7 @@
 # URL: https://www.nomisweb.co.uk/census/2011/ks501uk
 # Licence: Open Government Licence
 
-df <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_1510_1.data.csv?date=latest&geography=989862149...989862169&cell=1&measures=20301&select=date_name,geography_name,geography_code,cell_name,measures_name,obs_value,obs_status_name") %>% 
+df <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_1510_1.data.csv?date=latest&geography=989862149...989862169&cell=6&measures=20301&select=date_name,geography_name,geography_code,cell_name,measures_name,obs_value,obs_status_name") %>% 
   select(area_code = GEOGRAPHY_CODE,
          area_name = GEOGRAPHY_NAME,
          value = OBS_VALUE) %>% 
