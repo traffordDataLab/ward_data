e17d02a822085dcf72e15781a7ffddd34fab1b09
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-12-14 16:37:48 +0000

    Crime and anti-social behaviour

---
+++ community_safety/README.md
@@ -0,0 +1,10 @@
+## Community safety
+
+| Indicator     | Period        | Geography     | Source        | &nbsp;        | &nbsp;         |
+|:------------- |:------------- |:------------- |:------------- |:------------- | :------------- |
+| Anti-social behaviour | 2017-11 to 2018-10 | Electoral ward | Home Office | [data](data/anti-social_behaviour.csv) |  [code](code/anti-social_behaviour.R) |
+| Bicycle theft | 2017-11 to 2018-10 | Electoral ward | Home Office | [data](data/bicycle_theft.csv) |  [code](code/bicycle_theft.R) |
+| Burglary | 2017-11 to 2018-10 | Electoral ward | Home Office | [data](data/burglary.csv) |  [code](code/burglary.R) |
+| Robbery | 2017-11 to 2018-10 | Electoral ward | Home Office | [data](data/robbery.csv) |  [code](code/robbery.R) |
+| Vehicle crime | 2017-11 to 2018-10 | Electoral ward | Home Office | [data](data/vehicle_crime.csv) |  [code](code/vehicle_crime.R) |
+| Violence and sexual offences | 2017-11 to 2018-10 | Electoral ward | Home Office | [data](data/violence_and_sexual_offences.csv) |  [code](code/violence_and_sexual_offences.R) |
---
+++ community_safety/code/anti-social_behaviour.R
@@ -0,0 +1,43 @@
+# Community Safety: Anti-social behaviour, 2017-11 to 2018-10 #
+
+# Source: Home Office
+# URL: https://data.police.uk/
+# Licence: Open Government Licence
+# Method: Incidents as a rate per 1,000 residents
+
+library(tidyverse) ; library(sf) ; library(readxl)
+
+wards <- st_read("https://opendata.arcgis.com/datasets/07194e4507ae491488471c84b23a90f2_0.geojson") %>% 
+  filter(wd17cd %in% paste0("E0", seq(5000819, 5000839, 1))) %>%
+  select(area_code = wd17cd, area_name = wd17nm) 
+
+url <- "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/wardlevelmidyearpopulationestimatesexperimental/mid2017sape20dt8/sape20dt8mid2017ward2017syoaestimatesunformatted1.zip"
+download.file(url, dest = "sape20dt8mid2017ward2017syoaestimatesunformatted1.zip")
+unzip("sape20dt8mid2017ward2017syoaestimatesunformatted1.zip", exdir = ".")
+file.remove("sape20dt8mid2017ward2017syoaestimatesunformatted1.zip")
+
+pop <- read_excel("SAPE20DT8-mid-2017-ward-2017-syoa-estimates-unformatted.xls", sheet = 4, skip = 3) %>% 
+  filter(`Local Authority` == 'Trafford') %>% 
+  select(area_code = `Ward Code 1`, pop = `All Ages`)
+
+df <- dir(pattern = "*.csv") %>% 
+  map(read_csv) %>%
+  reduce(rbind) %>% 
+  filter(`Crime type` == "Anti-social behaviour",
+         grepl("Trafford", `LSOA name`)) %>% 
+  select(`Crime type`, Longitude, Latitude) %>% 
+  st_as_sf(crs = 4326, coords = c("Longitude", "Latitude")) %>% 
+  st_intersection(wards) %>% 
+  st_set_geometry(value = NULL) %>% 
+  group_by(area_code, area_name) %>% 
+  summarise(n = n()) %>% 
+  left_join(pop, by = "area_code") %>% 
+  mutate(value = round((n/pop)*1000, 1),
+         period = "2017-10 to 2018-11",
+         indicator = "Anti-social behaviour",
+         measure = "rate",
+         unit = "incidents") %>% 
+  select(area_code, area_name, indicator, period, measure, unit, value)
+
+write_csv(df, "../anti-social_behaviour.csv")
+
