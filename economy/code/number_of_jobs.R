0ecf638a05a959d2ed91e9a5c5d89261e1a71c47
James Austin <james.austin@trafford.gov.uk>
2019-01-09 16:48:13 +0000

    Corrected the path to save the data file.

--- economy/code/number_of_jobs.R
+++ economy/code/number_of_jobs.R
@@ -6,14 +6,14 @@
 
 library(tidyverse)
 
-df <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_190_1.data.csv?geography=E05000819...E05000839&date=latest&public_private=0&employment_status=1&measures=20100&select=date_name,geography_name,geography_code,public_private_name,employment_status_name,measures_name,obs_value,obs_status_name") %>% 
+df <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_190_1.data.csv?geography=E05000819...E05000839&date=latest&public_private=0&employment_status=1&measures=20100&select=date_name,geography_name,geography_code,public_private_name,employment_status_name,measures_name,obs_value,obs_status_name") %>%
   mutate(period = "2017",
          indicator = "Number of jobs",
          measure = "count",
-         unit = "employees") %>% 
-  select(area_code = GEOGRAPHY_CODE, 
-         area_name = GEOGRAPHY_NAME, 
-         indicator, period, measure, unit, 
+         unit = "employees") %>%
+  select(area_code = GEOGRAPHY_CODE,
+         area_name = GEOGRAPHY_NAME,
+         indicator, period, measure, unit,
          value = OBS_VALUE)
 
-write_csv(df, "../number_of_jobs.csv")
+write_csv(df, "../data/number_of_jobs.csv")
