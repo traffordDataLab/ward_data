97fa0abff1606e9d8ab408edba5e9f4058a84b98
James Austin <james.austin@trafford.gov.uk>
2019-01-11 16:37:46 +0000

    Amended the path to save the data file to the correct locationin the code files and normalised the measure values for consistency.

--- labour_market/code/claimant_rate.R
+++ labour_market/code/claimant_rate.R
@@ -7,15 +7,15 @@
 
 library(tidyverse) ; library(zoo)
 
-df <-read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_162_1.data.csv?geography=E05000819...E05000839&date=latest&gender=0&age=0&measure=2&measures=20100&select=date_name,geography_name,geography_code,gender_name,age_name,measure_name,measures_name,obs_value,obs_status_name") %>% 
-  select(area_code = GEOGRAPHY_CODE, 
-         area_name = GEOGRAPHY_NAME, 
+df <-read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_162_1.data.csv?geography=E05000819...E05000839&date=latest&gender=0&age=0&measure=2&measures=20100&select=date_name,geography_name,geography_code,gender_name,age_name,measure_name,measures_name,obs_value,obs_status_name") %>%
+  select(area_code = GEOGRAPHY_CODE,
+         area_name = GEOGRAPHY_NAME,
          period = DATE_NAME,
-         value = OBS_VALUE) %>% 
+         value = OBS_VALUE) %>%
   mutate(period = as.Date(as.yearmon(period, "%B %Y")),
          indicator = "Claimant rate",
          measure = "proportion",
-         unit = "persons") %>% 
+         unit = "persons") %>%
   select(area_code, area_name, indicator, period, measure, unit, value)
 
-write_csv(df, "../claimant_rate.csv")
+write_csv(df, "../data/claimant_rate.csv")
--- labour_market/code/professional_occupations.R
+++ labour_market/code/professional_occupations.R
@@ -5,21 +5,21 @@
 # Licence: Open Government Licence
 # Method: (Managers, directors and senior officials OR Professional occupations)/All usual residents aged 16 to 74 in employment)*100
 
-df <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_794_1.data.csv?date=latest&geography=1140857093...1140857113&c_sex=0&c_occpuk11_1=0,1,4&c_age=0&measures=20100&select=date_name,geography_name,geography_code,c_sex_name,c_occpuk11_1_name,c_age_name,measures_name,obs_value,obs_status_name") %>% 
-  select(area_code = GEOGRAPHY_CODE, area_name = GEOGRAPHY_NAME, type = C_OCCPUK11_1_NAME, n = OBS_VALUE) %>% 
-  mutate(type = 
+df <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_794_1.data.csv?date=latest&geography=1140857093...1140857113&c_sex=0&c_occpuk11_1=0,1,4&c_age=0&measures=20100&select=date_name,geography_name,geography_code,c_sex_name,c_occpuk11_1_name,c_age_name,measures_name,obs_value,obs_status_name") %>%
+  select(area_code = GEOGRAPHY_CODE, area_name = GEOGRAPHY_NAME, type = C_OCCPUK11_1_NAME, n = OBS_VALUE) %>%
+  mutate(type =
            factor(case_when(
              type == "All categories: Occupation" ~ "employed",
              type %in% c("1 : Managers, directors and senior officials",
-                         "2 : Professional occupations") ~ "professionals"))) %>% 
-  group_by(area_code, area_name, type) %>% 
-  summarise(n = sum(n)) %>% 
-  spread(type, n) %>% 
+                         "2 : Professional occupations") ~ "professionals"))) %>%
+  group_by(area_code, area_name, type) %>%
+  summarise(n = sum(n)) %>%
+  spread(type, n) %>%
   mutate(value = round((professionals/employed)*100, 1),
          period = "2011",
          indicator = "Professional occupations",
          measure = "percentage",
-         unit = "persons") %>% 
+         unit = "persons") %>%
   select(area_code, area_name, indicator, period, measure, unit, value)
 
-write_csv(df, "../professional_occupations.csv")
+write_csv(df, "../data/professional_occupations.csv")
