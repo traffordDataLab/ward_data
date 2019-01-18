1a8d5e059515f42bc691a6e584f56b13207fb04d
rcatlord <henrypartridge@henrys-mbp.tht.local>
2019-01-18 15:10:43 +0000

    Replaced ward codes

--- health/code/male_healthy_life_expectancy.R
+++ health/code/male_healthy_life_expectancy.R
@@ -6,16 +6,20 @@
 
 library(tidyverse)
 
+lookup <- read_csv("https://www.trafforddatalab.io/spatial_data/lookups/administrative_lookup.csv") %>% 
+  filter(lad17nm == "Trafford") %>% 
+  select(area_code = wd17cd, area_name = wd17nm)
+
 df <- read_csv("https://www.ons.gov.uk/visualisations/dvc479/scatter-wards/data.csv", skip = 10) %>% 
   filter(`Local authority name` == "Trafford",
          Sex == "Male") %>% 
-  select(area_code = `2011 Census Ward code`,
-         area_name = `2011 Census Ward name`,
+  select(area_name = `2011 Census Ward name`,
          value = `HLE (years)`) %>% 
   mutate(period = "2009-2013",
          indicator = "Male healthy life expectancy",
          measure = "count",
          unit = "persons") %>% 
+  left_join(lookup, by = "area_name") %>% 
   select(area_code, area_name, indicator, period, measure, unit, value)
 
 write_csv(df, "../male_healthy_life_expectancy.csv")
--- labour_market/code/professional_occupations.R
+++ labour_market/code/professional_occupations.R
@@ -5,14 +5,18 @@
 # Licence: Open Government Licence
 # Method: (Managers, directors and senior officials OR Professional occupations)/All usual residents aged 16 to 74 in employment)*100
 
+lookup <- read_csv("https://www.trafforddatalab.io/spatial_data/lookups/administrative_lookup.csv") %>% 
+  filter(lad17nm == "Trafford") %>% 
+  select(area_code = wd17cd, area_name = wd17nm)
+
 df <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_794_1.data.csv?date=latest&geography=1140857093...1140857113&c_sex=0&c_occpuk11_1=0,1,4&c_age=0&measures=20100&select=date_name,geography_name,geography_code,c_sex_name,c_occpuk11_1_name,c_age_name,measures_name,obs_value,obs_status_name") %>% 
-  select(area_code = GEOGRAPHY_CODE, area_name = GEOGRAPHY_NAME, type = C_OCCPUK11_1_NAME, n = OBS_VALUE) %>% 
+  select(area_name = GEOGRAPHY_NAME, type = C_OCCPUK11_1_NAME, n = OBS_VALUE) %>% 
   mutate(type = 
            factor(case_when(
              type == "All categories: Occupation" ~ "employed",
              type %in% c("1 : Managers, directors and senior officials",
                          "2 : Professional occupations") ~ "professionals"))) %>% 
-  group_by(area_code, area_name, type) %>% 
+  group_by(area_name, type) %>% 
   summarise(n = sum(n)) %>% 
   spread(type, n) %>% 
   mutate(value = round((professionals/employed)*100, 1),
@@ -20,6 +24,7 @@ df <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_794_1.data.csv?date
          indicator = "Professional occupations",
          measure = "percentage",
          unit = "persons") %>% 
+  left_join(lookup, by = "area_name") %>% 
   select(area_code, area_name, indicator, period, measure, unit, value)
 
 write_csv(df, "../professional_occupations.csv")
