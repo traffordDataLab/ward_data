53c0ce0d5a0746e3e0e9f4ee1bff872c4df95c07
rcatlord <henrypartridge@henrys-mbp.tht.local>
2019-09-06 16:21:46 +0100

    Life expectancy (2013-2017)

--- health/code/female_life_expectancy.R
+++ health/code/female_life_expectancy.R
@@ -1,4 +1,4 @@
-## Health: Female life expectancy at birth, 2011-2015 ##
+## Health: Female life expectancy at birth, 2013-2017 ##
 
 # Source: ONS
 # URL: http://www.localhealth.org.uk/
@@ -6,13 +6,10 @@
 
 library(tidyverse)
 
-df <- read_csv("LocalHealth_All_indicators_Ward_data.csv") %>%
-  filter(`Indicator Name` == "Life expectancy at birth, (upper age band 90+)",
-         Sex == "Female") %>%
-  select(area_code = `Area Code`,
-         area_name = `Area Name`,
-         value = Value) %>%
-  mutate(period = "2011 to 2015",
+df <- read_csv("life_exp.csv") %>% 
+  filter(sex == "Female") %>%
+  select(area_code, area_name, value = age) %>%
+  mutate(period = "2013 to 2017",
          indicator = "Female life expectancy at birth",
          measure = "Years",
          unit = "Females") %>%
