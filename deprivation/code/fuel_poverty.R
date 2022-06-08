8414b208eb9e8a02cd36d46ab695f820cbb73a34
IGRiosTC <iris.garciarios@trafford.gov.uk>
2022-06-08 10:53:21 +0100

    Update Fuel poverty

--- deprivation/code/fuel_poverty.R
+++ deprivation/code/fuel_poverty.R
@@ -13,20 +13,20 @@ lookup <- read_csv("https://opendata.arcgis.com/datasets/500d4283cbe54e3fa7f3583
   select(LSOA11CD, area_code = WD17CD, area_name = WD17NM)
 
 tmp <- tempfile(fileext = ".xlsx")
-GET(url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/882071/fuel-poverty-sub-regional-tables-2020-2018-data.xlsx",
+GET(url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1081191/sub-regional-fuel-poverty-2022-tables.xlsx",
     write_disk(tmp))
 
-df <- read_excel(tmp, sheet = 6, skip = 2) %>%
+df <- read_excel(tmp, sheet = 7, skip = 2) %>%
   filter(`LA Code` == "E08000009") %>%
   select(LSOA11CD = `LSOA Code`,
-         fuel_poor = `Number of households in fuel poverty1`,
-         households = `Number of households1`) %>%
+         fuel_poor = `Number of households in fuel poverty`,
+         households = `Number of households`) %>%
   left_join(., lookup, by = "LSOA11CD") %>%
   group_by(area_code, area_name) %>%
   summarise(fuel_poor = sum(fuel_poor),
             households = sum(households),
             value = round((fuel_poor/households)*100, 1)) %>%
-  mutate(period = "2018",
+  mutate(period = "2020",
          indicator = "Fuel poverty",
          measure = "Percentage",
          unit = "Households") %>%
