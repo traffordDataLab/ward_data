1228499280514d63b81d1a9371d2714031e214f2
IGRiosTC <iris.garciarios@trafford.gov.uk>
2022-07-20 18:13:38 +0100

    Update median house prices

--- housing/code/median_property_prices.R
+++ housing/code/median_property_prices.R
@@ -6,7 +6,7 @@
 
 library(tidyverse) ; library(readxl) ; library(jsonlite)
 
-url <- "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/housing/datasets/medianpricepaidbywardhpssadataset37/yearendingjune2020/hpssadataset37medianpricepaidbyward.zip"
+url <- "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/housing/datasets/medianpricepaidbywardhpssadataset37/yearendingdecember2021/hpssadataset37medianpricepaidbyward.zip"
 download.file(url, dest = "hpssadataset37medianpricepaidbyward.zip")
 unzip("hpssadataset37medianpricepaidbyward.zip", exdir = ".")
 file.remove("hpssadataset37medianpricepaidbyward.zip")
@@ -17,14 +17,14 @@ codes <- fromJSON(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/r
   distinct(attributes.WD18CD) %>% 
   pull(attributes.WD18CD) 
 
-df <- read_xls("HPSSA Dataset 37 - Median price paid by ward.xls", sheet = 6, skip = 4) %>% 
-  select(area_name = `Ward name`, area_code = `Ward code`, value = `Year ending Dec 2018`) %>% 
+df <- read_xls("HPSSA Dataset 37 - Median price paid by ward.xls", sheet = 6, skip = 5) %>% 
+  select(area_name = `Ward name`, area_code = `Ward code`, value = `Year ending Dec 2021`) %>% 
   filter(area_code %in% codes) %>% 
   mutate(value = as.integer(value),
-         period = "2020",
+         period = "2021",
          indicator = "Median property prices",
          measure = "Median",
-         unit = "Housholds") %>%
+         unit = "Pounds") %>%
   select(area_code, area_name, indicator, period, measure, unit, value)
 
 write_csv(df, "../data/median_property_prices.csv")
