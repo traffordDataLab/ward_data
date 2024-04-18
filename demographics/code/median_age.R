294659b6eeba59ba99e5a273092ae0a87a7a19aa
IGRiosTC <32395357+IGRiosTC@users.noreply.github.com>
2024-04-18 14:52:14 +0100

    Reintroduce median age  for period 2022

--- demographics/code/median_age.R
+++ demographics/code/median_age.R
@@ -1,22 +1,32 @@
-# Demographics: Median age, 2019 #
+# Demographics: Median age, 2022 #
 
-# Source: ONS 2019 Mid-Year Population Estimates
+# Source: Mid-year estimates 2022. Ward-level population estimates (official statistics in development
 # URL: https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/wardlevelmidyearpopulationestimatesexperimental
 # Licence: Open Government Licence
 
+library(httr) ; library(readxl) ; library(tidyverse)
+
+tmp <- tempfile(fileext = ".xlsx")
 
 
-library(tidyverse)
+GET(url = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/wardlevelmidyearpopulationestimatesexperimental/mid2021andmid2022/sapewardstablefinal.xlsx",
+    write_disk(tmp))
 
-row <- read_csv("https://www.nomisweb.co.uk/api/v01/dataset/NM_2010_1.data.csv?geography=1656750701...1656750715,1656750717,1656750716,1656750718...1656750721&date=latest&gender=0&c_age=101...191&measures=20100") 
-
-df <- row %>%
-  select(area_code = "GEOGRAPHY_CODE", area_name = "GEOGRAPHY_NAME", age = C_AGE_SORTORDER, Count = OBS_VALUE) %>%
+df <- read_xlsx(tmp, sheet = 8, skip = 3) %>%
+  filter(`LAD 2023 Code` == "E08000009") %>%
+  pivot_longer(Total:M90, names_to = "age", values_to = "value") %>%
+  mutate(period = as.Date("2022-06-30"))%>%
+  filter(age != "Total") %>%
+  separate(age, into = c("sex", "age"), sep = 1) %>%
+  mutate(sex = case_match(sex,"F"~"Females","M"~"Males")) %>%
+  pivot_wider(names_from = "sex", values_from = value) %>%
+  mutate(Persons = Females + Males) %>%
   mutate(age = as.integer(age)) %>%
+  rename(area_code = `Ward 2023 Code`, area_name = `Ward 2023 Name`) %>%
   group_by(area_code, area_name, age) %>%
-  summarise(Count) %>%
-  summarise(value = age[max(which(cumsum(Count)/sum(Count) <= 0.5))]) %>%
-  mutate(period = as.Date("2020-06-30", format = '%Y-%m-%d'),
+  summarise(Persons) %>%
+  summarise(value = age[max(which(cumsum(Persons)/sum(Persons) <= 0.5))]) %>%
+  mutate(period = as.Date("2022-06-30", format = '%Y-%m-%d'),
          indicator = "Median age",
          measure = "Median",
          unit = "Persons") %>%
