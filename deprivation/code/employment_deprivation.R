1927c75ced89511a2de85adc27da2d7d4afc7103
IGRiosTC <32395357+IGRiosTC@users.noreply.github.com>
2026-03-10 13:03:28 +0000

    Adding Indices of Deprivation 2025 in 10 categories

---
+++ deprivation/code/employment_deprivation.R
@@ -0,0 +1,46 @@
+# English Indices of Deprivation 2025 #
+# Indices of Deprivation: Employment#
+
+# Source: Ministry of Housing, Communities and Local Government
+# Publisher URL: https://www.gov.uk/government/statistics/english-indices-of-deprivation-2025
+# Licence: Open Government Licence 3.0
+
+# MHCLG does not publish the Indices of Deprivation at ward level. 
+# Ward level data produced according to the Appendix A. How to aggregate to different geographies of the English Indices of Deprivation 2019 Research report
+
+library(sf) ; library(tidyverse) ; library(janitor)
+
+la<-"Trafford"
+
+#df <- read_csv("https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/833982/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators.csv") %>%
+df <- read_csv("https://assets.publishing.service.gov.uk/media/691ded56d140bbbaa59a2a7d/File_7_IoD2025_All_Ranks_Scores_Deciles_Population_Denominators.csv") %>%
+
+  clean_names() %>%
+  filter(`local_authority_district_name_2024`==la) %>% 
+  select(lsoa21cd = "lsoa_code_2021", score = "employment_score_rate", population = "working_age_population_18_66_for_use_with_employment_deprivation_domain_mid_2022") %>% 
+  mutate(indicator="Indices of Deprivation: Employment")
+
+# LSOA to ward lookup #
+
+# Source: ONS Open Geography Portal 
+# Publisher URL: http://geoportal.statistics.gov.uk/
+# Licence: Open Government Licence 3.0
+
+# Best-fit lookup between LSOAs and wards
+#lookup <- read_csv("https://opendata.arcgis.com/datasets/8c05b84af48f4d25a2be35f1d984b883_0.csv") %>% 
+
+lookup <- fromJSON(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LSOA21_WD25_LAD25_EW_LU_v2/FeatureServer/0/query?where=LAD25NM%20%3D%20'", URLencode(toupper("Trafford"), reserved = TRUE), "'&outFields=LSOA21CD,LSOA21NM,WD25CD,WD25NM,LAD25CD,LAD25NM&outSR=4326&f=json"), flatten = TRUE) %>% 
+  pluck("features") %>% 
+  as_tibble() %>% 
+  select(lsoa21cd = attributes.LSOA21CD, area_code = attributes.WD25CD, area_name = attributes.WD25NM)
+
+#IoD lsoa to ward
+
+iod_ward <- left_join(df, lookup, by = "lsoa21cd") %>%
+  group_by(area_code, area_name, indicator) %>%
+  summarise(ward_score=sum(score*population)/sum(population)) %>%
+  ungroup %>%
+  mutate(period="2025",unit ="Persons", measure = "Percentage", value = round(ward_score * 100, digits = 1)) %>%
+  select(area_code, area_name, indicator, period, measure, unit, value)
+
+write_csv (iod_ward, "../data/employment_deprivation.csv")
